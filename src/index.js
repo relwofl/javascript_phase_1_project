c3e8501c6296d497f11dfb9a81a2ad34b80a7238
Luke Fowler <luciusfow@gmail.com>
2022-01-24 17:24:49 -0500

    SAVE

--- src/index.js
+++ src/index.js
@@ -1,1 +1,61 @@
-console.log("loaded")
+const locationForm = document.getElementById("location_form")
+const weatherInput = document.getElementById("location")
+KEY = "12e944034ac0209fc552d13c52d70eb8"
+
+locationForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+ 
+zip_code = weatherInput.value || "10010"
+console.log(zip_code)
+const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?zip=${zip_code},us&appid=${KEY}&units=imperial`
+    
+fetch(BASE_URL)
+    .then(res => res.json())
+    .then(weather => {
+        debugger
+        // console.log(weather)
+        let weatherARray = weather
+        renderWeather(weather)
+    })
+    weatherInput.value = ''
+})
+
+function renderWeather(weather) {
+    const displayWeather = document.getElementById("location_list")
+    const liTag = document.createElement('li')
+    let btn = document.createElement('button')
+    let btn1 = document.createElement('button')
+
+    btn.addEventListener('click', () => {
+        liTag.remove()
+    })
+
+    btn1.addEventListener('click', () => { 
+        const pTag3 = document.createElement('p')
+        pTag3.innerText = weather.wind.speed + " MPH"
+        if(liTag.childElementCount >=6){
+            liTag.tagName.innerHTML = ''
+        } else {
+            liTag.appendChild(pTag3)
+        }
+        // liTag.appendChild(pTag3)
+        console.log(pTag3)
+    })
+   
+    btn.textContent = 'Remove'
+    btn1.textContent = 'Add Wind'
+    const pTag = document.createElement('p')
+    pTag.innerText = weather.name
+
+    const pTag1 = document.createElement('p')
+    pTag1.innerText = weather.main.temp + " °F"
+
+    const pTag2 = document.createElement('p')
+    pTag2.innerText = weather.weather[0].description
+
+    liTag.append(pTag, pTag1, pTag2, btn, btn1 )
+    displayWeather.appendChild(liTag)
+}
+
+
+// add li button to replace li with forecast or add wind speed
